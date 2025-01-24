8a6b9bfa6c01b51a674cd5773514983ec7e4dbd5
M Ulmer <mju9999@gmail.com>
2025-01-23 21:21:56 -0800

    Add functions to support calculator operations

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 "use strict";
 
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+function operate(firstOperand, secondOperand, operator) {
+    return operator(firstOperand, secondOperand);
+}
+
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = add;
+
 let button = document.querySelector("button");
 button.addEventListener("click", () => console.log("clicked"));

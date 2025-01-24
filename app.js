860b708309fcd44c1936afd08b5e3bfd7af7db66
M Ulmer <mju9999@gmail.com>
2025-01-24 15:36:35 -0800

    Add clear and operator button click listeners
    The operator buttons don't yet WORK -- they set the relevant
    operator function but there's not yet logic to CALL said function.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 "use strict";
 
+// Number obtained by manually entering digits until the display was filled.
+// Is this hacky? Yes. Will it need to be changed every time the font size or
+// display width changes? Also yes. 
+// The alternative would be to declare the font size of the display as the
+// root font size and use em units to define everything, then redefine the 
+// button font size. If this were a production project, I would do that.
+// But it's not. So I'm going to keep the constant.
+const MAX_DISPLAY_DIGITS = 11;
+// ugh users can just keep multiplying and hit overflow :((
+const MAX_ENTRY_DIGITS = 8;
+
 const add = function (a, b) {
   return a + b;
 };
@@ -22,7 +33,7 @@ function operate(firstOperand, secondOperand, operator) {
 
 let firstOperand = 0;
 let secondOperand = 0;
-let operator = add;
+let operator = undefined;
 
 let currentTextEntry = "";
 
@@ -34,3 +45,31 @@ numberButtons.forEach((button) =>
     inputText.textContent = currentTextEntry;
   })
 );
+
+document.querySelectorAll(".operator").forEach((button) =>
+  button.addEventListener("click", () => {
+    switch (button.textContent) {
+        case '+':
+            operator = add;
+            break;
+        case '-':
+            operator = subtract;
+            break;
+        case 'X':
+            operator = multiply;
+            break;
+        case '/':
+            operator = divide;
+            break;
+        default:
+            operator = undefined;
+            log.console.error("Unexpected operator button.");
+    }
+    firstOperand = currentTextEntry;
+  })
+);
+
+document.querySelector(".clear-button").addEventListener("click", () => {
+    currentTextEntry = "";
+    inputText.textContent = currentTextEntry;
+  });

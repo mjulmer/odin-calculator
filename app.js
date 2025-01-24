010bc63b94821f1a48e8e0e386ef94d419ad4a32
M Ulmer <mju9999@gmail.com>
2025-01-24 11:30:14 -0800

    Add functionality to display number button press in input box.
    Operator and clear buttons don't do anything yet, and there's no
    guardrail for numbers too large for the input box overflowing.

--- app.js
+++ app.js
@@ -17,12 +17,20 @@ const divide = function (a, b) {
 };
 
 function operate(firstOperand, secondOperand, operator) {
-    return operator(firstOperand, secondOperand);
+  return operator(firstOperand, secondOperand);
 }
 
 let firstOperand = 0;
 let secondOperand = 0;
 let operator = add;
 
-let button = document.querySelector("button");
-button.addEventListener("click", () => console.log("clicked"));
+let currentTextEntry = "";
+
+const inputText = document.querySelector(".input-text");
+let numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    currentTextEntry = currentTextEntry + button.textContent;
+    inputText.textContent = currentTextEntry;
+  })
+);
